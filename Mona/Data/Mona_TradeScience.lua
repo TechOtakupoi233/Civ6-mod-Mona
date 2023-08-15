74c701f619e5a8a3c9241147925252ae167d935f
TechOtakupoi233 <slkj104@gmail.com>
2023-08-16 00:09:39 +0800

    Lots of updates
    Changed Civilization Icon
    Added UU
    Add gold transfer script

--- Mona/Data/Mona_TradeScience.lua
+++ Mona/Data/Mona_TradeScience.lua
@@ -1,16 +1,29 @@
 include("GameCapabilities");
+
+DebugON = true
 -- 回合计数器
 function TurnCounter()
     -- 获取当前回合数，if回合数能被5整除
     if (Game.GetCurrentGameTurn() ~= nil and Game.GetCurrentGameTurn() % 5 == 0 ) then
-        MonaTradeScience()
+        if DebugON then print("Turn " .. Game.GetCurrentGameTurn()) end
+        --遍历玩家，如果发现玩家具备“伟大的占星术士”特性，则将其加入MonaIDs表，且使OtherPlayers表从AlivePlayers表中获取数据时排除。
+        AlivePlayers = PlayerManager.GetAliveMajorIDs()
+        MonaIDs, OtherPlayers = {}, AlivePlayers
+        if DebugON then print("AlivePlayers: " .. table.concat(AlivePlayers, ",")) end
+        for i, playerID in pairs(AlivePlayers) do
+            if (HasTrait("TRAIT_GREAT_ASTROLOGIST", playerID)) then
+                table.insert(MonaIDs, playerID)
+                table.remove(OtherPlayers, i)
+            end
+        end
+        if DebugON then print("Found Mona PlayerID: " .. table.concat(MonaIDs, ",")) end
+        MonaTradeScience(MonaIDs, OtherPlayers)
     end
 end
+
 -- 钱换瓶
-function MonaTradeScience()
-    -- 检查玩家是否具备“伟大的占星术士”特性
-    for i, playerID in pairs(PlayerManager.GetAliveMajorIDs()) do
-    if (not HasTrait("TRAIT_GREAT_ASTROLOGIST", playerID)) then return; end
+function MonaTradeScience(MonaIDs, OtherPlayers)
+    for i, playerID in pairs(MonaIDs) do
         local pPlayer = Players[playerID]
         if pPlayer ~= nil then
         -- 获取金币余额并计算转换的科技值
@@ -19,6 +32,8 @@ function MonaTradeScience()
         -- 转科技和钱
         pPlayer:GetTechs():ChangeCurrentResearchProgress(TradingScience)
         pPlayer:GetTreasury():ChangeGoldBalance(0 - TradingGold)
+        if DebugON then print("Player " .. playerID .. " is trading " .. TradingScience .. " Science to " .. TradingGold .. " Gold") end
+        MonaFindHighestSciencePlayer(OtherPlayers, TradingGold)
         -- 获取首都坐标
         pCapital = pPlayer:GetCities():GetCapitalCity()
         CapitalPlotX = pCapital:GetX()
@@ -28,13 +43,27 @@ function MonaTradeScience()
         end
     end
 end
+
+--寻找场上除自身以外科技值最高的玩家
+function MonaFindHighestSciencePlayer(OtherPlayers, TradingGold)
+    if OtherPlayers == nil then print("No OtherPlayers found! MonaIDs:" .. table.concat(MonaIDs, ",")) return end
+    --获取OtherPlayers表中所有playerID所对应的科技值及其索引值
+    local PlayersScience = {}
+    for i, playerID in pairs(OtherPlayers) do PlayersScience[#OtherPlayers] = Players[playerID]:GetTechs():GetScienceYield() end
+    --计算最大值对应的PlayerID
+    maxSciencePlayerID = Getmaxnum(PlayersScience)
+    Players[maxSciencePlayerID]:GetTreasury():ChangeGoldBalance(TradingGold)
+    if DebugON then print("Sending " .. TradingGold .. " Gold to Player " .. maxSciencePlayerID) end
+end
+
 -- 研究返点
 function MonaResearchReward(ePlayer, eTech)
     -- 检查完成研究eTech的玩家是否具备“伟大的占星术士”特性
-    if (not HasTrait("TRAIT_GREAT_ASTROLOGIST", ePlayer)) then return; end
+    if (not HasTrait("TRAIT_GREAT_ASTROLOGIST", ePlayer)) then
+    else
     -- 获取完成研究eTech所消耗的科技点，计算返点金额
     local pPlayer = Players[ePlayer]
-    RewardGold = pPlayer:GetTechs():GetResearchCost(eTech) / 3
+    RewardGold = pPlayer:GetTechs():GetResearchCost(eTech) / 2
     -- 返点到账
     pPlayer:GetTreasury():ChangeGoldBalance(RewardGold)
     -- 获取首都坐标
@@ -55,6 +84,19 @@ function MonaResearchReward(ePlayer, eTech)
     notificationData.AlwaysUnique = true;
     --notificationData.ShowIcon = false;
     NotificationManager.SendNotification(ePlayer, ResearchRewardNotification, notificationData)
+    end
+end
+
+function Getmaxnum(a)
+    local mi = 1             -- maximum index
+    local m = a[mi]          -- maximum value
+    for i,val in ipairs(a) do
+       if val > m then
+           mi = i
+           m = val
+       end
+    end
+    return mi
 end
 
 Events.TurnBegin.Add(TurnCounter)
